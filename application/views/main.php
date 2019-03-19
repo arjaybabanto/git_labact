756ede365ccf82eaf48eb8692541a773f7e03a0d
Rheyjan Carl Babanto <arjaybabanto@gmail.com>
2019-03-19 20:53:08 +0800

    web dev commited

---
+++ application/views/main.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My first git</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<p> My First Git </p>
+</body>
+<script type="text/javascript" href="assets/js/js.js"></script>
+</html>
